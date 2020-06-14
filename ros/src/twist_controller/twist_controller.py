aa09c8798d2e62ae3f52f88e25bda6007daff152
Øystein lasson <oystein.lasson@gmail.com>
2020-06-14 13:30:44 +0200

    Working on twist controller

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,17 +1,34 @@
 import rospy
 from pid import PID
+from lowpass import LowPassFilter
+from yaw_controller import YawController
 
 # Constants
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
+TAU = 0.5 # Cutoff frequency
+T_SAMPLE = 0.02 # Sample time
+
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, vehicle_mass ,fuel_capacity ,brake_deadband ,decel_limit  ,accel_limit  ,wheel_radius ,wheel_base ,steer_ratio ,max_lat_accel ,max_steer_angle, min_speed):
         """
         Lesson 8: DBW Walkthrough in Project: Programming a real Self-Driving Car was very useful here
         """
+        
+        
         # TODO: Implement
+        
+        # Member variables
+        self.vehicle_mass = vehicle_mass 
+        self.fuel_capacity = fuel_capacity
+        self.brake_deadband = brake_deadband 
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_radius
+        
+        # PID
         Kp = 0.3 # Proportional gain
         Ki = 0.1 # Intergral gain
         Kd = 0.0 # Derivative gain
@@ -19,11 +36,24 @@ class Controller(object):
         min_throttle = 0.0
         max_throttle = 0.0
         
+        # Init controllers
         self.throttle_controller = PID(Kp, Ki, Kd, min_throttle, max_throttle)
+        self.yaw_controller = YawController(wheel_base,steer_ratio ,max_lat_accel , max_steer_angle, min_speed)
         
-        pass
+        # Init lowpass filter
+        self.velocity_lowpass = LowPassFilter(TAU, T_SAMPLE)
+        
+        self.last_time = rospy.get_time()
+        
+        
 
-    def control(self, *args, **kwargs):
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
+        if not dbw_enabled:
+            self.throttle_controller.reset()
+            return 0.0, 0.0, 0.0
+        
+        current_vel = self.vel_lpf.filt(current_vel)        
+        
         return 1., 0., 0.
